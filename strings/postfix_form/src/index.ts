1fd807619bfdd647e2c77648829ac66e9a585084
Justin <justinknueppel@gmail.com>
2020-05-30 16:35:02 -0400

    Extract functions from convert

--- strings/postfix_form/src/index.ts
+++ strings/postfix_form/src/index.ts
@@ -3,23 +3,29 @@ const convert = (infix: string): string => {
   if (tokens.length === 0) {
     return ""
   }
-  let postfix = ""
-  let firstTerm = tokens.shift()!;
-  while (tokens.length > 1) {
-    const operator = tokens.shift()!
-    const secondTerm = tokens.shift()!
-    postfix = formatExpression(firstTerm, secondTerm, operator)
-    firstTerm = postfix
+  let postfix = tokens.shift()!;
+  while (atLeastTwoTokensRemaining(tokens)) {
+    postfix = addNextTwoTokensToPostfix(postfix, tokens);
   }
   return postfix
 };
 
-const formatExpression = (term1: string, term2: string, operator: string): string => {
-  return `${term1} ${term2} ${operator}`
-}
-
 const getTokens = (infix: string): Array<string> => {
   return infix.split(" ")
+}
+
+const atLeastTwoTokensRemaining = (tokens: Array<string>): boolean => {
+  return tokens.length >= 2;
+}
+
+const addNextTwoTokensToPostfix = (postfix: string, tokens: Array<string>): string => {
+  const operator = tokens.shift()!
+  const secondTerm = tokens.shift()!
+  return formatExpression(postfix, secondTerm, operator)
+}
+
+const formatExpression = (term1: string, term2: string, operator: string): string => {
+  return `${term1} ${term2} ${operator}`
 }
 
 export default convert;
