97d460b34a7bb0e3ea151dbe65ed8d8a82594afb
Justin <justinknueppel@gmail.com>
2020-06-01 13:42:38 -0400

    add tests with order of operations

--- strings/postfix_form/test/index.test.ts
+++ strings/postfix_form/test/index.test.ts
@@ -13,27 +13,39 @@ describe("Convert infix to postfix", () => {
   });
 
   it("Given x * y return x y *", () => {
-    const infix = "1 * 2"
-    expect(convert(infix)).to.equal("1 2 *")
-  })
+    const infix = "1 * 2";
+    expect(convert(infix)).to.equal("1 2 *");
+  });
 
   it("Given y * x return y x *", () => {
-    const infix = "2 * 1"
-    expect(convert(infix)).to.equal("2 1 *")
-  })
+    const infix = "2 * 1";
+    expect(convert(infix)).to.equal("2 1 *");
+  });
 
   it("Given x / y return x y /", () => {
-    const infix = "1 / 2"
-    expect(convert(infix)).to.equal("1 2 /")
-  })
+    const infix = "1 / 2";
+    expect(convert(infix)).to.equal("1 2 /");
+  });
 
   it("Given x - y return x y -", () => {
-    const infix = "1 - 2"
-    expect(convert(infix)).to.equal("1 2 -")
-  })
+    const infix = "1 - 2";
+    expect(convert(infix)).to.equal("1 2 -");
+  });
 
   it("Given x + y + z return x y z + +", () => {
-    const infix = "1 + 2 + 3"
+    const infix = "1 + 2 + 3";
     expect(convert(infix)).to.equal("1 2 + 3 +");
-  })
+  });
+
+  it("Given x * y + z return x y * z +", () => {
+    const infix = "1 * 2 + 3";
+    const postfix = "1 2 * 3 +";
+    expect(convert(infix)).to.equal(postfix);
+  });
+
+  it("Given x + y * z return x y z * +", () => {
+    const infix = "1 + 2 * 3";
+    const postfix = "1 2 3 * +";
+    expect(convert(infix)).to.equal(postfix);
+  });
 });
