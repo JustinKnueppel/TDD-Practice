d5865da4fa1d41e98f3fe336b72d4d65660c1420
Justin <justinknueppel@gmail.com>
2020-06-03 15:05:05 -0400

    extract postfix from equals call in tests

--- strings/postfix_form/test/index.test.ts
+++ strings/postfix_form/test/index.test.ts
@@ -4,32 +4,38 @@ import convert from "../src/index";
 describe("Convert infix to postfix", () => {
   it("Given single addition expression, postfix the operator", () => {
     const infix = "1 + 2";
-    expect(convert(infix)).to.equal("1 2 +");
+    const postfix = "1 2 +"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Given single addition expression, order of terms does not change", () => {
     const infix = "2 + 1";
-    expect(convert(infix)).to.equal("2 1 +");
+    const postfix = "2 1 +"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Multiplication recognized as operator", () => {
     const infix = "1 * 2";
-    expect(convert(infix)).to.equal("1 2 *");
+    const postfix = "1 2 *"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Division recognized as operator", () => {
     const infix = "1 / 2";
-    expect(convert(infix)).to.equal("1 2 /");
+    const postfix = "1 2 /"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Subtraction recognized as operator", () => {
     const infix = "1 - 2";
-    expect(convert(infix)).to.equal("1 2 -");
+    const postfix = "1 2 -"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Given two expressions of same type, postfix operators sequentially", () => {
     const infix = "1 + 2 + 3";
-    expect(convert(infix)).to.equal("1 2 + 3 +");
+    const postfix = "1 2 + 3 +"
+    expect(convert(infix)).to.equal(postfix);
   });
 
   it("Given two expressions of different types in order of order of operations, postfix operators sequentially", () => {
