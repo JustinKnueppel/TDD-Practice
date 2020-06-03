036fd83c703d04622d36719481e594dde9ba18d2
Justin <justinknueppel@gmail.com>
2020-06-03 15:05:24 -0400

    Apply prettier formatting

--- strings/postfix_form/src/index.ts
+++ strings/postfix_form/src/index.ts
@@ -1,74 +1,83 @@
 const convert = (infix: string): string => {
-  const tokens = getTokens(infix)
+  const tokens = getTokens(infix);
   if (tokens.length === 0) {
-    return ""
+    return "";
   }
   while (atLeastTwoTokensRemaining(tokens)) {
-    const substitutionIndex = hasMultiplicationOrDivision(tokens) ? firstMultiplicationOrDivisionTermIndex(tokens) : 0
-    substituteExpression(tokens, substitutionIndex)
+    const substitutionIndex = hasMultiplicationOrDivision(tokens)
+      ? firstMultiplicationOrDivisionTermIndex(tokens)
+      : 0;
+    substituteExpression(tokens, substitutionIndex);
   }
-  return tokens[0]
+  return tokens[0];
 };
 
 const hasMultiplicationOrDivision = (tokens: Array<string>): boolean => {
-  return hasMultiplication(tokens) || hasDivision(tokens)
-}
+  return hasMultiplication(tokens) || hasDivision(tokens);
+};
 
 const hasMultiplication = (tokens: Array<string>): boolean => {
-  return getMultiplicationIndex(tokens) >= 0
-}
+  return getMultiplicationIndex(tokens) >= 0;
+};
 
 const hasDivision = (tokens: Array<string>): boolean => {
-  return getDivisionIndex(tokens) >= 0
-}
+  return getDivisionIndex(tokens) >= 0;
+};
 
 const getDivisionIndex = (tokens: Array<string>): number => {
-  return tokens.indexOf("/")
-}
+  return tokens.indexOf("/");
+};
 
-const firstMultiplicationOrDivisionTermIndex = (tokens: Array<string>): number => {
-  const multiplicationIndex = firstMultiplicationTermIndex(tokens)
-  const divisionIndex = firstDivisionTermIndex(tokens)
+const firstMultiplicationOrDivisionTermIndex = (
+  tokens: Array<string>
+): number => {
+  const multiplicationIndex = firstMultiplicationTermIndex(tokens);
+  const divisionIndex = firstDivisionTermIndex(tokens);
 
-  if (multiplicationIndex < 0)
-    return divisionIndex
+  if (multiplicationIndex < 0) return divisionIndex;
 
-  if (divisionIndex < 0)
-    return multiplicationIndex
+  if (divisionIndex < 0) return multiplicationIndex;
 
-  return Math.min(multiplicationIndex, divisionIndex)
-}
+  return Math.min(multiplicationIndex, divisionIndex);
+};
 
 const firstDivisionTermIndex = (tokens: Array<string>): number => {
-  return getDivisionIndex(tokens) - 1
-}
+  return getDivisionIndex(tokens) - 1;
+};
 
 const firstMultiplicationTermIndex = (tokens: Array<string>): number => {
   return getMultiplicationIndex(tokens) - 1;
-}
+};
 
 const getMultiplicationIndex = (tokens: Array<string>): number => {
-  return tokens.indexOf("*")
-}
+  return tokens.indexOf("*");
+};
 
-const substituteExpression = (tokens: Array<string>, startIndex: number): void => {
-  const firstTerm = tokens[startIndex]
-  const operator = tokens[startIndex + 1]
-  const secondTerm = tokens[startIndex + 2]
+const substituteExpression = (
+  tokens: Array<string>,
+  startIndex: number
+): void => {
+  const firstTerm = tokens[startIndex];
+  const operator = tokens[startIndex + 1];
+  const secondTerm = tokens[startIndex + 2];
   const formattedExpression = formatExpression(firstTerm, secondTerm, operator);
-  tokens.splice(startIndex, 3, formattedExpression)
-}
+  tokens.splice(startIndex, 3, formattedExpression);
+};
 
 const getTokens = (infix: string): Array<string> => {
-  return infix.split(" ")
-}
+  return infix.split(" ");
+};
 
 const atLeastTwoTokensRemaining = (tokens: Array<string>): boolean => {
   return tokens.length >= 2;
-}
+};
 
-const formatExpression = (term1: string, term2: string, operator: string): string => {
-  return `${term1} ${term2} ${operator}`
-}
+const formatExpression = (
+  term1: string,
+  term2: string,
+  operator: string
+): string => {
+  return `${term1} ${term2} ${operator}`;
+};
 
 export default convert;
--- strings/postfix_form/test/index.test.ts
+++ strings/postfix_form/test/index.test.ts
@@ -4,37 +4,37 @@ import convert from "../src/index";
 describe("Convert infix to postfix", () => {
   it("Given single addition expression, postfix the operator", () => {
     const infix = "1 + 2";
-    const postfix = "1 2 +"
+    const postfix = "1 2 +";
     expect(convert(infix)).to.equal(postfix);
   });
 
   it("Given single addition expression, order of terms does not change", () => {
     const infix = "2 + 1";
-    const postfix = "2 1 +"
+    const postfix = "2 1 +";
     expect(convert(infix)).to.equal(postfix);
   });
 
   it("Multiplication recognized as operator", () => {
     const infix = "1 * 2";
-    const postfix = "1 2 *"
+    const postfix = "1 2 *";
     expect(convert(infix)).to.equal(postfix);
   });
 
   it("Division recognized as operator", () => {
     const infix = "1 / 2";
-    const postfix = "1 2 /"
+    const postfix = "1 2 /";
     expect(convert(infix)).to.equal(postfix);
   });
 
   it("Subtraction recognized as operator", () => {
     const infix = "1 - 2";
-    const postfix = "1 2 -"
+    const postfix = "1 2 -";
     expect(convert(infix)).to.equal(postfix);
   });
 
   it("Given two expressions of same type, postfix operators sequentially", () => {
     const infix = "1 + 2 + 3";
-    const postfix = "1 2 + 3 +"
+    const postfix = "1 2 + 3 +";
     expect(convert(infix)).to.equal(postfix);
   });
 
@@ -51,10 +51,10 @@ describe("Convert infix to postfix", () => {
   });
 
   it("Division operates before addition", () => {
-    const infix = "1 + 2 / 3"
-    const postfix = "1 2 3 / +"
-    expect(convert(infix)).to.equal(postfix)
-  })
+    const infix = "1 + 2 / 3";
+    const postfix = "1 2 3 / +";
+    expect(convert(infix)).to.equal(postfix);
+  });
 
   it("Multiplication operates before subtraction", () => {
     const infix = "1 - 2 * 3";
@@ -63,20 +63,20 @@ describe("Convert infix to postfix", () => {
   });
 
   it("Division operates before subtraction", () => {
-    const infix = "1 - 2 / 3"
-    const postfix = "1 2 3 / -"
-    expect(convert(infix)).to.equal(postfix)
-  })
+    const infix = "1 - 2 / 3";
+    const postfix = "1 2 3 / -";
+    expect(convert(infix)).to.equal(postfix);
+  });
 
   it("Given multiplication then division, operate in order", () => {
-    const infix = "1 * 2 / 3"
-    const postfix = "1 2 * 3 /"
-    expect(convert(infix)).to.equal(postfix)
-  })
+    const infix = "1 * 2 / 3";
+    const postfix = "1 2 * 3 /";
+    expect(convert(infix)).to.equal(postfix);
+  });
 
   it("Given division then multiplication, operate in order", () => {
-    const infix = "1 / 2 * 3"
-    const postfix = "1 2 / 3 *"
-    expect(convert(infix)).to.equal(postfix)
-  })
+    const infix = "1 / 2 * 3";
+    const postfix = "1 2 / 3 *";
+    expect(convert(infix)).to.equal(postfix);
+  });
 });
