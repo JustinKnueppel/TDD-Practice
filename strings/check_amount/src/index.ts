0219f6cb339d54eb2054a7452e065b8751bf084e
Justin <justinknueppel@gmail.com>
2020-06-08 14:20:00 -0400

    Parse check amounts less than $10

--- strings/check_amount/src/index.ts
+++ strings/check_amount/src/index.ts
@@ -1,10 +1,44 @@
 const CENTS_PER_DOLLAR = 100
 
 const checkAmountToString = (checkAmount: number): string => {
+  const dollars = dollarsFromCheckAmount(checkAmount)
+  const dollarString = parseOnesDigit(dollars)
+  const formattedDollarString = capitalizeFirstLetter(dollarString)
   const cents = integerCents(checkAmount)
   const centsString = getCentsString(cents);
-  return `Zero and ${centsString} dollars`;
+  return `${formattedDollarString} and ${centsString} dollars`;
 };
+
+const dollarsFromCheckAmount = (checkAmount: number): number => {
+  return Math.floor(checkAmount)
+}
+
+const capitalizeFirstLetter = (str: string): string => {
+  const firstLetter = str[0]
+  const restOfString = str.substring(1, str.length)
+  return firstLetter.toUpperCase() + restOfString
+}
+
+const parseOnesDigit = (dollars: number): string => {
+  if (dollars === 0) {
+    return "zero"
+  }
+  const onesStrings = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  ]
+
+  const onesDigit = dollars % 10
+  return onesStrings[onesDigit]
+}
 
 const integerCents = (checkAmount: number): number => {
   return (checkAmount * CENTS_PER_DOLLAR) % CENTS_PER_DOLLAR
--- strings/check_amount/test/index.test.ts
+++ strings/check_amount/test/index.test.ts
@@ -3,27 +3,83 @@ import checkAmountToString from "../src/index";
 
 describe("Check to string", () => {
   it("First letter is capitalized", () => {
-    const amount = 0.00;
+    const amount = 0.0;
     const checkString = checkAmountToString(amount);
     const firstCharacter = checkString[0];
     expect(firstCharacter).to.equal(firstCharacter.toUpperCase());
   });
 
   it("Amount with no decimal gives 00/100 cents", () => {
-    const amount = 0.00
-    const checkString = "Zero and 00/100 dollars"
-    expect(checkAmountToString(amount)).to.equal(checkString)
-  })
+    const amount = 0.0;
+    const checkString = "Zero and 00/100 dollars";
+    expect(checkAmountToString(amount)).to.equal(checkString);
+  });
 
   it("Cents change when decimal changes", () => {
-    const amount = 0.01
+    const amount = 0.01;
     const checkString = "Zero and 01/100 dollars";
-    expect(checkAmountToString(amount)).to.equal(checkString)
-  })
+    expect(checkAmountToString(amount)).to.equal(checkString);
+  });
 
   it("99 cents gives correct cents", () => {
-    const amount = 0.99
+    const amount = 0.99;
     const checkString = "Zero and 99/100 dollars";
-    expect(checkAmountToString(amount)).to.equal(checkString)
-  })
+    expect(checkAmountToString(amount)).to.equal(checkString);
+  });
+
+  describe("Single digit dollars", () => {
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+
+    it("One dollar has one for the dollar string", () => {
+      const amount = 1.0;
+      const checkString = "One and 00/100 dollars";
+      expect(checkAmountToString(amount)).to.equal(checkString);
+    });
+  });
 });
