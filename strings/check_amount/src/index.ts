a35d317cd63cec07be55e116d2f1e737cac6e096
Justin <justinknueppel@gmail.com>
2020-06-08 14:01:40 -0400

    Make cents adjust correctly

--- strings/check_amount/src/index.ts
+++ strings/check_amount/src/index.ts
@@ -1,5 +1,23 @@
+const CENTS_PER_DOLLAR = 100
+
 const checkAmountToString = (checkAmount: number): string => {
-  return "Zero and 00/100 dollars";
+  const cents = integerCents(checkAmount)
+  const centsString = getCentsString(cents);
+  return `Zero and ${centsString} dollars`;
 };
+
+const integerCents = (checkAmount: number): number => {
+  return (checkAmount * CENTS_PER_DOLLAR) % CENTS_PER_DOLLAR
+}
+
+const getCentsString = (cents: number): string => {
+  const twoDigitCents = getTwoDigitCents(cents);
+  return `${twoDigitCents}/${CENTS_PER_DOLLAR}`
+}
+
+const getTwoDigitCents = (cents: number): string => {
+  const prefix = cents < 10 ? "0" : ""
+  return prefix + cents
+}
 
 export default checkAmountToString;
--- strings/check_amount/test/index.test.ts
+++ strings/check_amount/test/index.test.ts
@@ -14,4 +14,16 @@ describe("Check to string", () => {
     const checkString = "Zero and 00/100 dollars"
     expect(checkAmountToString(amount)).to.equal(checkString)
   })
+
+  it("Cents change when decimal changes", () => {
+    const amount = 0.01
+    const checkString = "Zero and 01/100 dollars";
+    expect(checkAmountToString(amount)).to.equal(checkString)
+  })
+
+  it("99 cents gives correct cents", () => {
+    const amount = 0.99
+    const checkString = "Zero and 99/100 dollars";
+    expect(checkAmountToString(amount)).to.equal(checkString)
+  })
 });
